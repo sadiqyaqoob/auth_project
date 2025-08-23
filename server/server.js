c9ba978ed14418fe9a4070a3331d83737c813def
Sadiq <sadiqyaqoon070@gmail.com>
2025-08-23 07:51:55 +0500

    Added MongoDB connection and fixed env URI

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB()
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ Credential: true }));
+
+app.get("/", (req, res) => res.send("Api working"));
+
+app.listen(port, () => console.log(`server started on port:${port}`));
