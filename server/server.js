7addde87c6b8465f52d58f25365e50fe9785e865
Sadiq <sadiqyaqoon070@gmail.com>
2025-08-26 17:58:52 +0500

    api to get user info

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
+import userAuth from "./middleware/userAuth.js";
+import userRouter from "./routes/userRouter.js";
 
 
 const app = express();
@@ -18,6 +20,7 @@ app.use(cors({ Credential: true }));
 //API Endpoint
 app.get("/", (req, res) => res.send("Api working"));
 app.use('/api/auth' , authRouter)
+app.use('/api/user', userRouter)
 
 
 app.listen(port, () => console.log(`server started on port:${port}`));
